187b9bdc0e412f84cb46673b27a4c8727740e8a0
Samantha Callie <Hoomee90@gmail.com>
2024-05-22 14:25:06 -0700

    create description, date and price order properties

--- VendorTracker.Tests/ModelTests/OrderTests.cs
+++ VendorTracker.Tests/ModelTests/OrderTests.cs
@@ -30,5 +30,59 @@ namespace VendorTracker.Tests
 			newOrder.Title = newTitle;
 			Assert.AreEqual(newTitle, newOrder.Title);
 		}
+		
+		[TestMethod]
+		public void GetDescription_ReturnsDescription_String()
+		{
+			string description = "red";
+			Order newOrder = new("apples", description);
+			string result = newOrder.Description;
+			Assert.AreEqual(description, result);
+		}
+		
+		[TestMethod]
+		public void SetDescription_SetsValueOfDescription_Void()
+		{
+			Order newOrder = new("cheese", "red");
+			string newDescription = "loves you";
+			newOrder.Description = newDescription;
+			Assert.AreEqual(newDescription, newOrder.Description);
+		}
+		
+		[TestMethod]
+		public void GetDate_ReturnsDate_String()
+		{
+			string date = "2013-10-26";
+			Order newOrder = new("pumpkin", "goes round and round", date);
+			string result = newOrder.Date;
+			Assert.AreEqual(date, result);
+		}
+		
+		[TestMethod]
+		public void SetDate_SetsValueOfDate_Void()
+		{
+			Order newOrder = new("melon", "when it splits, a sweet dream is created", "2013-10-26");
+			string newDate = "2014-04-02";
+			newOrder.Date = newDate;
+			Assert.AreEqual(newDate, newOrder.Date);
+		}
+		
+		[TestMethod]
+		public void GetPrice_ReturnsPrice_Double()
+		{
+			double price = 5;
+			Order newOrder = new("cake", "bitter", "2013-10-26", price);
+			double result = newOrder.Price;
+			Assert.AreEqual(price, result);
+		}
+		
+		[TestMethod]
+		public void SetPrice_SetsValueOfPrice_Void()
+		{
+			Order newOrder = new("cake", "the dream of a cat", "2013-10-26", 5);
+			double newPrice = 6.5;
+			newOrder.Price = newPrice;
+			Assert.AreEqual(newPrice, newOrder.Price);
+		}
 	}
 }
--- VendorTracker/Models/Order.cs
+++ VendorTracker/Models/Order.cs
@@ -3,9 +3,16 @@ namespace VendorTracker.Models
 	public class Order
 	{
 		public string Title { get; set; }
-		public Order(string title)
+		public string Description { get; set; }
+		public double Price { get; set; }
+		public string Date { get; set; }
+		
+		public Order(string title, string description = "", string date = "0000-00-00", double price = 0)
 		{
 			Title = title;
+			Description = description;
+			Price = price;
+			Date = date;
 		}
 	}
 }
